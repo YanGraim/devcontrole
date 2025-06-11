0a04880a8c2de5877fc10e453a05cd85789ffd50
YanGraim <yangraim@gmail.com>
2025-06-11 11:07:46 -0300

    feat: lista clientes no banco

--- src/app/dashboard/customer/components/card/index.tsx
+++ src/app/dashboard/customer/components/card/index.tsx
@@ -1,14 +1,23 @@
-export function CardCustomer() {
+import { CustomerProps } from "@/utils/customer.type";
+
+
+
+export function CardCustomer({ customer }: { customer: CustomerProps }) {
     return (
         <article className="flex flex-col bg-gray-100 border border-slate-200 p-2 gap-2 rounded-lg hover:-translate-y-1 hover:shadow-xl transition duration-300 select-none">
             <h2>
-                <span className="font-bold">Nome:</span> Mercado do Doda
+                <span className="font-bold">Nome:</span> {customer.name}
             </h2>
             <p>
-                <span className="font-bold">Email:</span> teste@teste.com
+                <span className="font-bold">Email:</span> {customer.email}
             </p>
+            {customer.address && (
+                <p>
+                    <span className="font-bold">Endereço:</span> {customer.address}
+                </p>
+            )}
             <p>
-                <span className="font-bold">Telefone:</span> (xx) xxxxx-xxxx
+                <span className="font-bold">Telefone:</span> {customer.phone}
             </p>
             <button className="bg-red-500 px-6 rounded text-white mt-2 self-start cursor-pointer">Deletar</button>
         </article>
--- src/app/dashboard/customer/page.tsx
+++ src/app/dashboard/customer/page.tsx
@@ -4,6 +4,7 @@ import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { CardCustomer } from "./components/card";
+import prisma from "@/lib/prisma";
 
 
 export default async function Customer() {
@@ -12,20 +13,24 @@ export default async function Customer() {
     if (!session || !session.user) {
         redirect("/");
     }
+
+    const customers = await prisma.customer.findMany({
+        where: {
+            userId: session.user.id
+        }
+    })
+
     return (
         <Container>
             <main className="mt-9 mb-2">
                 <div className="flex items-center justify-between">
                     <h1 className="text-3xl font-bold">Meus clientes</h1>
-                    <Link href={"/dashboard/customer/new"} className="text-white bg-blue-500 rounded px-4 py-1">Novo chamado</Link>
+                    <Link href={"/dashboard/customer/new"} className="text-white bg-blue-500 rounded px-4 py-1">Novo cliente</Link>
                 </div>
                 <section className="grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3 mt-4">
-                    <CardCustomer />
-
-                    <CardCustomer />
-
-                    <CardCustomer />
-
+                    {customers.map((customer) => (
+                        <CardCustomer key={customer.id} customer={customer} />
+                    ))}
                 </section>
             </main>
         </Container>

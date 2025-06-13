d7704ca4ab76d1d6808b898b1b8699e306d19da2
YanGraim <yangraim@gmail.com>
2025-06-13 11:06:38 -0300

    style: altera alguns estilos e icones

--- src/app/dashboard/components/ticket/index.tsx
+++ src/app/dashboard/components/ticket/index.tsx
@@ -1,6 +1,6 @@
 import { CustomerProps } from "@/utils/customer.type";
 import { TicketProps } from "@/utils/ticket.type";
-import { FiFile, FiTrash2 } from "react-icons/fi";
+import { FiCheckSquare, FiFile } from "react-icons/fi";
 
 interface TicketItemProps {
     ticket: TicketProps;
@@ -10,13 +10,13 @@ interface TicketItemProps {
 export function TicketItem({ customer, ticket }: TicketItemProps) {
     return (
         <>
-            <tr className="bg-slate-100 border-b-2 border-b-slate-300 h-14 last:border-b-0 hover:bg-gray-200 duration-300 select-none">
+            <tr className="bg-slate-100 border-b-2 border-b-slate-300 h-14 last:border-b-0 hover:bg-gray-300 duration-300 select-none">
                 <td className="text-left pl-1">{customer?.name}</td>
                 <td className="text-center">{ticket.created_at?.toLocaleDateString("pt-BR")}</td>
-                <td className="text-center"><span className="bg-green-500 px-2 py-1 rounded">{ticket.status}</span></td>
+                <td className="text-center"><span className="bg-green-500 text-white px-2 py-1 rounded">{ticket.status}</span></td>
                 <td className="text-right">
                     <button className="cursor-pointer mr-2">
-                        <FiTrash2 size={24} color="#ef4444" />
+                        <FiCheckSquare size={24} color="#131313" />
                     </button>
                     <button className="cursor-pointer">
                         <FiFile size={24} color="#3b82f6" />
